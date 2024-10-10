ed6f24699790a1a19571f693525fd0d5c3a9fa52
serhii <ockar2018@gmail.com>
2024-10-10 20:41:45 +0300

    add start endpoints for project

---
+++ theater/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path, include
+from rest_framework import routers
+
+from theater.views import (
+    GenreViewSet, ActorViewSet, PlayViewSet,
+)
+
+router = routers.DefaultRouter()
+router.register("genres", GenreViewSet)
+router.register("actors", ActorViewSet)
+router.register("plays", PlayViewSet)
+
+urlpatterns = [path("", include(router.urls))]
+
+app_name = "theater"
