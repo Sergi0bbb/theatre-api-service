3ca6c03f204d866b921276d90f70f9576d028c9d
serhii <ockar2018@gmail.com>
2024-10-15 16:56:49 +0300

    add ordering and ticket validation

--- theatre/models.py
+++ theatre/models.py
@@ -1,5 +1,11 @@
+import os
+import uuid
+from typing import Callable
+
 from django.conf import settings
 from django.db import models
+from rest_framework.exceptions import ValidationError
+from django.utils.text import slugify
 
 
 class Genre(models.Model):
@@ -13,19 +19,31 @@ class Actor(models.Model):
     first_name = models.CharField(max_length=150)
     last_name = models.CharField(max_length=150)
 
+    def __str__(self) -> str:
+        return f"{self.first_name} {self.last_name}"
+
     @property
     def full_name(self):
         return f"{self.first_name} {self.last_name}"
 
-    def __str__(self) -> str:
-        return f"{self.first_name} {self.last_name}"
+
+def movie_image_file_path(instance, filename):
+    _, extension = os.path.splitext(filename)
+    filename = f"{slugify(instance.title)}-{uuid.uuid4()}{extension}"
+
+    return os.path.join("uploads/plays/", filename)
 
 
 class Play(models.Model):
     title = models.CharField(max_length=150, unique=True)
     description = models.TextField()
+    duration = models.PositiveIntegerField(default="60")
     genres = models.ManyToManyField(Genre, related_name="plays", blank=True)
     actors = models.ManyToManyField(Actor, related_name="plays", blank=True)
+    image = models.ImageField(null=True, upload_to=movie_image_file_path)
+
+    class Meta:
+        ordering = ["title"]
 
     def __str__(self) -> str:
         return self.title
@@ -35,6 +53,10 @@ class TheatreHall(models.Model):
     name = models.CharField(max_length=150, unique=True)
     rows = models.PositiveIntegerField()
     seats_in_row = models.PositiveIntegerField()
+
+    @property
+    def capacity(self) -> int:
+        return self.rows * self.seats_in_row
 
     def __str__(self) -> str:
         return self.name
@@ -63,8 +85,11 @@ class Reservation(models.Model):
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE
     )
 
+    class Meta:
+        ordering = ["-created_at"]
+
     def __str__(self) -> str:
-        return f"Reservation by {self.user.username} on {self.created_at}"
+        return f"Reservation on {self.created_at}"
 
 
 class Ticket(models.Model):
@@ -80,6 +105,45 @@ class Ticket(models.Model):
         related_name="tickets",
         on_delete=models.CASCADE
     )
+
+    class Meta:
+        ordering = ["row", "seat"]
+
+    @staticmethod
+    def validate_ticket(
+            row: int,
+            seat: int,
+            theatre_hall: TheatreHall,
+            error: Callable
+    ):
+
+        for ticket_attr_value, ticket_attr_name, hall_attr_name in [
+            (row, "row", "rows"),
+            (seat, "seat", "seats_in_row"),
+        ]:
+            max_value = getattr(theatre_hall, hall_attr_name)
+
+            if not (1 <= ticket_attr_value <= max_value):
+                raise error(
+                    {
+                        ticket_attr_name: (
+                            f"{ticket_attr_name.capitalize()} number must be "
+                            f"in available range: (1, {max_value})"
+                        )
+                    }
+                )
+
+    def clean(self):
+        Ticket.validate_ticket(
+            self.row,
+            self.seat,
+            self.performance.theatre_hall,
+            ValidationError
+        )
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        super().save(*args, **kwargs)
 
     def __str__(self) -> str:
         return f"{self.performance} (Row {self.row}, Seat {self.seat})"
